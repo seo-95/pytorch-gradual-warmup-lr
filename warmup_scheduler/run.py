bdf88ca0c48f3fd9d3a27267226a66bdf26778f7
seo-95 <matteo.senese@gmail.com>
2021-01-22 19:33:48 +0100

    Warmup granularity changed to step. Removed the need to perform a free optimizer zero_grad and step

--- warmup_scheduler/run.py
+++ warmup_scheduler/run.py
@@ -1,24 +1,31 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
-from torch.optim.lr_scheduler import StepLR, ExponentialLR
+from torch.optim.lr_scheduler import MultiStepLR
 from torch.optim.sgd import SGD
 
 from warmup_scheduler import GradualWarmupScheduler
-
 
 if __name__ == '__main__':
     model = [torch.nn.Parameter(torch.randn(2, 2, requires_grad=True))]
     optim = SGD(model, 0.1)
 
     # scheduler_warmup is chained with schduler_steplr
-    scheduler_steplr = StepLR(optim, step_size=10, gamma=0.1)
-    scheduler_warmup = GradualWarmupScheduler(optim, multiplier=1, warmup_epochs=5, post_warmup_scheduler=scheduler_steplr)
+    scheduler_steplr = MultiStepLR(optim, milestones=[300, 400], gamma=0.1)
+    scheduler_warmup = GradualWarmupScheduler(optim, multiplier=1, warmup_steps=200, post_warmup_scheduler=scheduler_steplr)
 
-    # this zero gradient update is needed to avoid a warning message, issue #8.
-    optim.zero_grad()
-    optim.step()
-
-    for epoch in range(1, 20):
-        scheduler_warmup.step()
-        print(epoch, optim.param_groups[0]['lr'])
-
-        optim.step()    # backward pass (update network)
+    lr_values = []
+    global_step = 0
+    for epoch in range(1, 10):
+        for step in range(1, 50):
+            global_step += 1
+            lr_values.append(optim.param_groups[0]['lr'])
+            optim.step()    # backward pass (update network)
+            scheduler_warmup.step()
+    plt.plot(np.arange(1, len(lr_values)+1), lr_values, color='blue')
+    plt.title('Learning rate trend')
+    plt.xlabel('Steps')
+    plt.ylabel('LR')
+    plt.savefig('lr.png')
+    print('result saved in lr.png')
--- warmup_scheduler/scheduler.py
+++ warmup_scheduler/scheduler.py
@@ -1,5 +1,4 @@
 from torch.optim.lr_scheduler import _LRScheduler
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class GradualWarmupScheduler(_LRScheduler):
@@ -9,63 +8,33 @@ class GradualWarmupScheduler(_LRScheduler):
     Args:
         optimizer (Optimizer): Wrapped optimizer.
         multiplier: target learning rate = base lr * multiplier if multiplier > 1.0. if multiplier = 1.0, lr starts from 0 and ends up with the base_lr.
-        total_epoch: target learning rate is reached at total_epoch, gradually
         after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
     """
 
-    def __init__(self, optimizer, multiplier, warmup_epochs, post_warmup_scheduler=None):
+    def __init__(self, optimizer, multiplier, warmup_steps, post_warmup_scheduler=None):
         self.multiplier = multiplier
         if self.multiplier < 1.:
             raise ValueError('multiplier should be greater thant or equal to 1.')
-        self.warmup_epochs = warmup_epochs
-        self.post_warmup_scheduler = post_warmup_scheduler
-        self.finished = False if warmup_epochs > 0 else True
+        self.wup_steps = warmup_steps
+        self.post_wup_scheduler = post_warmup_scheduler
         super(GradualWarmupScheduler, self).__init__(optimizer)
 
     def get_lr(self):
-        if self.last_epoch > self.warmup_epochs:
-            if self.post_warmup_scheduler:
-                if not self.finished:
-                    self.post_warmup_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
-                    self.finished = True
-                return self.post_warmup_scheduler.get_last_lr()
-            return [base_lr * self.multiplier for base_lr in self.base_lrs]
-
+        assert self._step_count <= self.wup_steps, 'Warmup scheduler called after last warmup step'
         if self.multiplier == 1.0:
-            return [base_lr * (float(self.last_epoch) / self.warmup_epochs) for base_lr in self.base_lrs]
+            return [base_lr * (float(self._step_count) / self.wup_steps) for base_lr in self.base_lrs]
         else:
-            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.warmup_epochs + 1.) for base_lr in self.base_lrs]
+            return [base_lr * ((self.multiplier - 1.) * self._step_count / self.wup_steps + 1.) for base_lr in self.base_lrs]
 
-    def step_ReduceLROnPlateau(self, metrics, epoch=None):
-        if epoch is None:
-            epoch = self.last_epoch + 1
-        self.last_epoch = epoch if epoch != 0 else 1  # ReduceLROnPlateau is called at the end of epoch, whereas others are called at beginning
-        if self.last_epoch <= self.warmup_epochs:
-            if self.last_epoch == self.total_epoch:
-                self.post_warmup_scheduler.best = metrics
-            if self.multiplier == 1.0:
-                warmup_lr = [base_lr * (float(self.last_epoch) / self.warmup_epochs) for base_lr in self.base_lrs]
-            else:
-                warmup_lr = [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.warmup_epochs + 1.) for base_lr in self.base_lrs]
-            for param_group, lr in zip(self.optimizer.param_groups, warmup_lr):
-                param_group['lr'] = lr
-
+    def step(self):
+        if self._step_count < self.wup_steps:
+            return super(GradualWarmupScheduler, self).step()
         else:
-            if epoch is None:
-                self.post_warmup_scheduler.step(metrics, None)
-            else:
-                self.post_warmup_scheduler.step(metrics, epoch - self.warmup_epochs)
-
-    def step(self, epoch=None, metrics=None):
-        if type(self.post_warmup_scheduler) != ReduceLROnPlateau:
-            if self.finished and self.post_warmup_scheduler:
-                if self._step_count != 0:
-                    #when warmup_epochs is set to 0, the post scheduler must wait one step
-                    # otherwise you will lose the first lr value.
-                    self.post_warmup_scheduler.step()
-                    self._last_lr = self.post_warmup_scheduler.get_last_lr()
-                self._step_count += 1
-            else:
-                return super(GradualWarmupScheduler, self).step()
-        else:
-            self.step_ReduceLROnPlateau(metrics, epoch)
+            if self._step_count == self.wup_steps:
+                # setup the post warmup scheduler if this is the first post warmup step
+                self.post_wup_scheduler.base_lrs    = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                self.post_wup_scheduler._step_count = self._step_count
+                self.post_wup_scheduler.last_epoch  = self.last_epoch
+            self.post_wup_scheduler.step()
+            self._step_count    += 1
+            self.last_epoch     += 1
